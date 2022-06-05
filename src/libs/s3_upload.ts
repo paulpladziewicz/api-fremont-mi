5eef73eaf83345dbf12009b48295c42bdc865329
paulpladziewicz <ppladziewicz@gmail.com>
2022-06-05 07:21:15 -0400

    included S3 lib

---
+++ src/libs/s3_upload.ts
@@ -0,0 +1,11 @@
+import { PutObjectCommand } from "@aws-sdk/client-s3"
+import { s3Client } from "./s3Client.js";
+
+export const run = async (bucketParams: any) => {
+  try {
+    const data = await s3Client.send(new PutObjectCommand(bucketParams));
+    return data;
+  } catch (err) {
+    console.log("Error", err);
+  }
+};
